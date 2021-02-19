21fe08bd518a64b49b16636728619fe5dd6adea2
Ricardo Taipe <rdavid28@hotmail.es>
2021-02-19 17:17:51 -0500

    setting place on board

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,27 @@
 class TicTacToe {
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
   play(x, y) {
     this.checkAxis(x);
     this.checkAxis(y);
+    this.setBox(x, y);
   }
 
   checkAxis(axis) {
     if (axis < 1 || axis > 3) {
-      throw new Error();
+      throw new Error("Outside board");
+    }
+  }
+
+  setBox(x, y) {
+    if (this.board[x - 1][y - 1] !== "") {
+      throw new Error("Box is occupied");
+    } else {
+      this.board[x - 1][y - 1] = "X";
     }
   }
 }
--- tictactoe.test.js
+++ tictactoe.test.js
@@ -1,13 +1,18 @@
-const TicTacToe = require('./tictactoe');
+const TicTacToe = require("./tictactoe");
 
 let ticTacToe = new TicTacToe();
 
-test('whenXOutsideBoardThenRuntimeException', () => {
-  expect(()=> ticTacToe.play(5,3)).toThrow(Error)
-  expect(()=> ticTacToe.play(-2,3)).toThrow(Error)
+test("whenXOutsideBoardThenException", () => {
+  expect(() => ticTacToe.play(5, 3)).toThrow(Error);
+  expect(() => ticTacToe.play(-2, 3)).toThrow(Error);
 });
 
-test('whenYOutsideBoardThenRuntimeException', () => {
-    expect(()=> ticTacToe.play(2,5)).toThrow(Error)
-    expect(()=> ticTacToe.play(2,-3)).toThrow(Error)
-  });
+test("whenYOutsideBoardThenException", () => {
+  expect(() => ticTacToe.play(2, 5)).toThrow(Error);
+  expect(() => ticTacToe.play(2, -3)).toThrow(Error);
+});
+
+test("whenOccupiedThenException", () => {
+  ticTacToe.play(2, 1);
+  expect(() => ticTacToe.play(2, 1)).toThrow(Error);
+});
